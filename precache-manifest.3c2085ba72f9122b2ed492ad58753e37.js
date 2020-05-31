366169031f49838b31cdf9bf3abf76ecbbe4ed3a
maruan-it <maryan.goroshko@ukr.net>
2020-05-31 19:40:42 +0200

    Updates

---
+++ precache-manifest.3c2085ba72f9122b2ed492ad58753e37.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7be8be527b73caf7ad06ec5cb6b47e7a",
+    "url": "/demo-socialNetwork/index.html"
+  },
+  {
+    "revision": "6981669c84ec5a4d29f0",
+    "url": "/demo-socialNetwork/static/css/4.81d20019.chunk.css"
+  },
+  {
+    "revision": "64ae77e77eee9239fd24",
+    "url": "/demo-socialNetwork/static/css/5.a3703684.chunk.css"
+  },
+  {
+    "revision": "730826b7a225571a3acc",
+    "url": "/demo-socialNetwork/static/css/6.1050bd78.chunk.css"
+  },
+  {
+    "revision": "9746ab0383f423465139",
+    "url": "/demo-socialNetwork/static/css/7.87ac1538.chunk.css"
+  },
+  {
+    "revision": "fa033d739ecaa054918a",
+    "url": "/demo-socialNetwork/static/css/main.3bc5703c.chunk.css"
+  },
+  {
+    "revision": "b42fb7066b1531d62284",
+    "url": "/demo-socialNetwork/static/js/0.a1927588.chunk.js"
+  },
+  {
+    "revision": "29e7f940b7ebaeea2b9a",
+    "url": "/demo-socialNetwork/static/js/3.b782066e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo-socialNetwork/static/js/3.b782066e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6981669c84ec5a4d29f0",
+    "url": "/demo-socialNetwork/static/js/4.f23d93bf.chunk.js"
+  },
+  {
+    "revision": "64ae77e77eee9239fd24",
+    "url": "/demo-socialNetwork/static/js/5.f47c0369.chunk.js"
+  },
+  {
+    "revision": "730826b7a225571a3acc",
+    "url": "/demo-socialNetwork/static/js/6.9668a38d.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/demo-socialNetwork/static/js/6.9668a38d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9746ab0383f423465139",
+    "url": "/demo-socialNetwork/static/js/7.0fb4d342.chunk.js"
+  },
+  {
+    "revision": "fa033d739ecaa054918a",
+    "url": "/demo-socialNetwork/static/js/main.6ff02e14.chunk.js"
+  },
+  {
+    "revision": "abe3a32285758bc62450",
+    "url": "/demo-socialNetwork/static/js/runtime-main.05dbd2c4.js"
+  },
+  {
+    "revision": "d5481c3678529ad207a144fc6054d788",
+    "url": "/demo-socialNetwork/static/media/5.d5481c36.svg"
+  }
+]);
